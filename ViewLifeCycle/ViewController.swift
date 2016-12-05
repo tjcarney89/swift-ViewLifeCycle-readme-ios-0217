bc0c29c7e8bd43c8dd57917e6dbf694223485874
TJ <tjcarney@stny.rr.com>
2016-12-05 18:01:32 -0500

    Completed Lifecycle

--- ViewLifeCycle/ViewController.swift
+++ ViewLifeCycle/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("viewDidAppear(_:) called")
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
